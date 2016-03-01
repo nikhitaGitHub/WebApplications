61906aad90d5bb2a80a39768de8b3668e6984f51
Nikhita Sareen <nikhitasareen@gmail.com>
2016-03-01 13:18:13 -0500

    Update GulpFile and Add image responsiveness
    compress images and add more gulp usage

--- ResponsiveDesignExample/gulpfile.js
+++ ResponsiveDesignExample/gulpfile.js
@@ -1,17 +1,42 @@
+/*eslint-env node*/
+
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 
-gulp.task('default', ['styles'], function() {
-	console.log("Hello");
-	gulp.watch('css/**/*.css', ['styles']).on('change', browserSync.reload);
-  	browserSync.init({
-    	server: "./"
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+
+
+gulp.task('default', ['copy-html', 'copy-images', 'cssStyle', 'images'], function() {
+	gulp.watch('css/**/*.css', ['cssStyle']).on('change', browserSync.reload);
+	gulp.watch('index.html', ['copy-html']).on('change', browserSync.reload);
+
+	browserSync.init({
+		server: './'
 	});
 	browserSync.stream();
 });
 
-gulp.task('styles', function() {
-	gulp.src('css/**/*.css')
+gulp.task('cssStyle', function() {
+	gulp.src('css/**/*.css', {'read': false})
 		.pipe(browserSync.stream());
+    });
+
+gulp.task('copy-html', function() {
+	gulp.src('./index.html')
+		.pipe(gulp.dest('./dist'));
 });
 
+gulp.task('copy-images', function() {
+	gulp.src('images/*')
+		.pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('images', function() {
+	return gulp.src('dist/img/*')
+        .pipe(imagemin({
+ 			progressive: true,
+         	use: [pngquant()]
+        }))
+        .pipe(gulp.dest('dist/images'));
+});
